3f0e56a803bfdc04bd6bb1f007b1f0bdd748f96f
satyajit090 <beherasatyajit090@gmail.com>
2020-07-07 08:49:33 +0530

    modification of api call

--- src/main/kotlin/com/demo/map/api/MapController.kt
+++ src/main/kotlin/com/demo/map/api/MapController.kt
@@ -12,46 +12,38 @@ import okhttp3.Request
 @RestController
 class MapController {
 
-	@GetMapping("/map/zone")
-	fun getRoutingZones(@RequestParam("countryCodes") countryCodes: String): String {
-		
-		
-		val url = "https://route.ls.hereapi.com/routing/7.2/getroutingzones.json?countryCodes="+countryCodes
-		val token ="eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiJOY3V3dFJTdEN3Z3lJVDMzZkdRRyIsImlhdCI6MTU5Mzg3NzkyMywiZXhwIjoxNTkzOTY0MzIzLCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLlYzSGNkeWJKM2Zwd2hWOE9LWlhPU0EuV0JJZ3EzNm5UTU5HeGI2c3VIOEtWb0plem1GUFk2Q21NaElKbkpzd0FFR3c3RHlDX2hYeENtZlRKcUNMLVl2LVo2X1h5TGRsVDAzczZWS2t2eHF4dzNqRlBGZlo2NHNrNTVudmpLUVFzSC1JWHBZZ1VhbURqUGx1SXJiRFliYzgudE5NT2EzM3h5Y0NMX1dCVXYwR0tHMXNNUzg4STNaazMzZ1ZLUWlveVBOVQ.ZM8vVpnpKXrLGt5MGIDQa0dSfFXkmxWxf6xNjLSHvMZycY1pNBBt6MDLyPkYbajJwfbtQUo2FlaDPT8rr5ZdWHUNHMsHFaKaQPddF88Zu2rR7p2veUUM3-XJq5y3mL1a5ILahXmQMYvzxMY79qtR0ne-47Jdsbg4XYb_AElHNzsZpC-Bi-clMXlpVNhzRnRwSixv6GW_ZHvrmWk9KkKMxzLdrL_KlAfdAbiYV9d_-4VQkJS50pFqQ0Eq8B0mhpICfIuqPy4OyKKipusMRMhD4DBHQ_N85i6N2E3ik6nHX1deKt6aDPAGwW69x04Q61jF0pALmw2-gQ88ut3YWenZmg"
-		
-		val client = OkHttpClient()
-		val request = Request.Builder()
-        .addHeader("Authorization", "Bearer "+token)
-        .url(url)
-        .build()
+	@GetMapping("/map")
+	fun getMap(@RequestParam("at") at: String): String {
 
-        val  response = client.newCall (request).execute()
+		val urlEatDrink = "https://places.ls.hereapi.com/places/v1/discover/explore?at=" + at + "&cat=eat-drink"
 
-        val body = response.body!!.string()
-		
-		return body;
-	}
-	
-	
-	@GetMapping("/map/evcharger")
-	fun getEvCharging(@RequestParam("prox") prox: String): String {
-		
-		
-		val url = "https://ev-v2.cit.cc.api.here.com/ev/stations.json?prox="+prox
-		val token ="eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiJOY3V3dFJTdEN3Z3lJVDMzZkdRRyIsImlhdCI6MTU5Mzg3NzkyMywiZXhwIjoxNTkzOTY0MzIzLCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLlYzSGNkeWJKM2Zwd2hWOE9LWlhPU0EuV0JJZ3EzNm5UTU5HeGI2c3VIOEtWb0plem1GUFk2Q21NaElKbkpzd0FFR3c3RHlDX2hYeENtZlRKcUNMLVl2LVo2X1h5TGRsVDAzczZWS2t2eHF4dzNqRlBGZlo2NHNrNTVudmpLUVFzSC1JWHBZZ1VhbURqUGx1SXJiRFliYzgudE5NT2EzM3h5Y0NMX1dCVXYwR0tHMXNNUzg4STNaazMzZ1ZLUWlveVBOVQ.ZM8vVpnpKXrLGt5MGIDQa0dSfFXkmxWxf6xNjLSHvMZycY1pNBBt6MDLyPkYbajJwfbtQUo2FlaDPT8rr5ZdWHUNHMsHFaKaQPddF88Zu2rR7p2veUUM3-XJq5y3mL1a5ILahXmQMYvzxMY79qtR0ne-47Jdsbg4XYb_AElHNzsZpC-Bi-clMXlpVNhzRnRwSixv6GW_ZHvrmWk9KkKMxzLdrL_KlAfdAbiYV9d_-4VQkJS50pFqQ0Eq8B0mhpICfIuqPy4OyKKipusMRMhD4DBHQ_N85i6N2E3ik6nHX1deKt6aDPAGwW69x04Q61jF0pALmw2-gQ88ut3YWenZmg"
-		
-		val client = OkHttpClient()
-		val request = Request.Builder()
-        .addHeader("Authorization", "Bearer "+token)
-        .url(url)
-        .build()
+		val urlEvCharging = "https://places.ls.hereapi.com/places/v1/discover/explore?at=" + at + "&cat=ev-charging-station"
+		val token =
+			"eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiJOY3V3dFJTdEN3Z3lJVDMzZkdRRyIsImlhdCI6MTU5NDA5MTA1NiwiZXhwIjoxNTk0MTc3NDU2LCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLlVMN2hWaWVfdlhRYldtdl84S1hoRFEuaEJ0cjJKYUdBNDZoVmNiQlFmZkFNUm9YV0NUQmlGMHNHbl83YkxrYi16MTJOTFUyNF9FRzBJbVc2UkpTS190NDhwM2lvbGR3dDIyR3hxLTUyWExnX0xDd2s3ekhYbkY3NWxpeHVJN0ZTeVlOeGNfRXk0ZW9TSkFCT20yLTFDblYuRVd6UHFHUDk3WTl5VFRzWkFudFJiV0FUSXVmU2hEQlZXTGo1b0NlMFVSdw.qQ8B3hzHqOBDrFeYROIWdF81R1VFoC3gCX9XDKXEqkkfi_-ocSiaCi2-X9dYHQqO1vhIZHc7Q2o2EqOgmgl2-KDw6gL0KUY6rTUi8Eo2MJ2MU1BftP9Z05UlGKj-2lISgxJqR6MzDv0rUsG6B6Wc6plCj6Wak5hU_kfsgRQ__JR0G3kPM0JEBcdHlQcqTJ5rhLhwgXgIyNQGTMqgp4vsw-Olv97_qOEgb-UXasYoyf6rxO0CqVB2JwVjgEhmdpwh6oP4ARKUYEbKpu8iK0Ehs2NH8_skBy95CONYgV8lbjk4zDcRb1ZmLx6fITdrJtrSdVPt_mDefpbquYeP7FGuGA"
 
-        val  response = client.newCall (request).execute()
+		val clientEatDrink = OkHttpClient()
+		val requestEatDrink = Request.Builder()
+			.addHeader("Authorization", "Bearer " + token)
+			.url(urlEatDrink)
+			.build()
 
-        val body = response.body!!.string()
-		
-		return body;
+		val responseEatDrink = clientEatDrink.newCall(requestEatDrink).execute()
+
+		val bodyEatDrink = responseEatDrink.body!!.string()
+
+
+		val clientEvCharging = OkHttpClient()
+		val requestEvCharging = Request.Builder()
+			.addHeader("Authorization", "Bearer " + token)
+			.url(urlEvCharging)
+			.build()
+
+		val responseEvCharging = clientEvCharging.newCall(requestEvCharging).execute()
+
+		val bodyEvCharging = responseEvCharging.body!!.string()
+
+		return bodyEatDrink + bodyEvCharging;
 	}
 
-	
+
 }
